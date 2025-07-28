c3d9497eaf0bc133ab5e5c160cb4d9008622d8f3
ritzzh <ritesh.singh.19a@gmail.com>
2025-07-28 13:26:42 +0530

    28 July: Added Map

--- jump-jelly/src/app/modules/map-core/watch-locations/watch-locations.ts
+++ jump-jelly/src/app/modules/map-core/watch-locations/watch-locations.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
+import maplibregl, { Map, Marker } from 'maplibre-gl';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-watch-locations',
@@ -7,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrl: './watch-locations.scss'
 })
 export class WatchLocations {
+  @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
 
+  map!: Map;
+  markers: Marker[] = [];
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (!isPlatformBrowser(this.platformId)) {
+      console.log("is returning")
+      return; // SSR protection
+    }
+
+    this.map = new maplibregl.Map({
+      container: this.mapContainer.nativeElement,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=uoGKddW6vUlPtzcTSXSJ`,
+      center: [77.5946, 12.9716],
+      zoom: 5
+    });
+
+    console.log("initiating map")
+
+    this.map.addControl(new maplibregl.NavigationControl());
+  }
+
+  addLocation(location: string): void {
+    const coords = location.split(',').map(Number);
+    if (coords.length === 2 && !isNaN(coords[0]) && !isNaN(coords[1])) {
+      const [lat, lng] = coords;
+      const marker = new maplibregl.Marker().setLngLat([lng, lat]).addTo(this.map);
+      this.markers.push(marker);
+      this.map.flyTo({ center: [lng, lat], zoom: 10 });
+    } else {
+      alert('Enter coordinates as "latitude,longitude"');
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.map) {
+      this.map.remove();
+    }
+  }
 }
